295d71ed6ce309a110c5038e19f15240318496f3
0xbul1 <diaoszhao@gmail.com>
2019-11-13 23:25:15 +0800

    zxx add

---
+++ src/network/request.js
@@ -0,0 +1,25 @@
+import axios from 'axios'
+
+export function request(config){
+  //创建axios的实例
+  const instance = axios.create({
+    baseURL : "http://123.207.32.32:8000/api/vip",
+    // baseURL :"http://106.54.54.237:8000/api/v1",
+    timeout: 5000
+  })
+  
+  //axios的拦截器
+  instance.interceptors.request.use(config => {
+    return config
+  }, err => {
+    console.log(err);
+  })
+
+  instance.interceptors.response.use(res => {
+    return res.data
+  },err => {
+    console.log(err);
+  })
+
+  return instance(config);
+}
---
+++ src/views/home/Home.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="home">
+    <nav-bar class="home-nav">
+      <div slot="center">购物街</div>
+    </nav-bar>
+  </div>
+</template>
+<script>
+  import NavBar from 'components/common/navbar/NavBar'
+  import { getHomeMultidata } from 'network/home'
+
+  export default {
+    name: "Home",
+    components: {
+      NavBar
+    },
+    data() {
+      return {
+        banners: [],
+        recommends: []
+      }
+    },
+    created() {
+
+      //請求多個數據
+      getHomeMultidata().then(res => {
+        this.banners = res.data.banner;
+        this.recommends = res.data.recommend;
+      })
+    },
+  }
+</script>
+<style scoped>
+  .home-nav {
+    background: var(--color-tint);
+    color: var(--color-background);
+  }
+</style>
