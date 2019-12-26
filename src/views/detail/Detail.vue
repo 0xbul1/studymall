a77b18c61a51caa0344a79c8055db5145b625a2f
0xbul1 <diaoszhao@gmail.com>
2019-12-26 16:42:50 +0800

    添加了商品详细信息 但是因为betterscroll的高度计算在图片没有加载完成时，所以华东还是存在bug。这次不用防抖，直接监听图片是否加载完

--- src/views/detail/Detail.vue
+++ src/views/detail/Detail.vue
@@ -5,6 +5,7 @@
       <detail-swiper :top-images="topImages" />
       <detail-basic-info :goods="goods" />
       <detail-shop-info :shop="shop" />
+      <detail-goods-info :detail-goods-info="detailGoodsInfo" />
     </scroll>
   </div>
 </template>
@@ -13,6 +14,8 @@ import DetailNavBar from "./childComps/DetailNavBar";
 import DetailSwiper from "./childComps/DetailSwiper";
 import DetailBasicInfo from "./childComps/DetailBasicInfo";
 import DetailShopInfo from "./childComps/DetailShopInfo";
+import DetailGoodsInfo from "./childComps/DetailGoodsInfo";
+
 import Scroll from "components/common/scroll/Scroll";
 
 import { getDetail, Goods, Shops } from "network/detail";
@@ -22,23 +25,26 @@ export default {
   data() {
     return {
       iid: null, //具体商品详情的id
-      topImages: [],
-      goods: {},
-      shop: {}
+      topImages: [], // 详情页轮播图片
+      goods: {}, //详情页商品数据
+      shop: {}, //详情页店铺数据
+      detailGoodsInfo: {} //店铺信息下面的商品描述和图片等详细信息
     };
   },
   created() {
     this.iid = this.$route.params.iid;
     getDetail(this.iid).then(res => {
+      //请求接口数据
       console.log(res);
       const data = res.result;
-      this.topImages = data.itemInfo.topImages; //详情页轮播图片
-      this.goods = new Goods( //详情页整合的商品数据
+      this.topImages = data.itemInfo.topImages;
+      this.goods = new Goods(
         data.itemInfo,
         data.columns,
         data.shopInfo.services
       );
       this.shop = new Shops(data.shopInfo);
+      this.detailGoodsInfo = data.detailInfo;
     });
   },
   components: {
@@ -46,6 +52,7 @@ export default {
     DetailSwiper,
     DetailBasicInfo,
     DetailShopInfo,
+    DetailGoodsInfo,
     Scroll
   }
 };
@@ -62,7 +69,6 @@ export default {
   height: calc(100% - 44px);
 }
 .nav {
-  /* -webkit-filter: ; */
   position: relative;
   background: #fff;
   z-index: 1;
